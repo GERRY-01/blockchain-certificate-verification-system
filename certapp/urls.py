a5c6179bf36d461f1c51d7f446af097e38afdb96
Gerry-01 <odhiambogerry010@gmail.com>
2025-08-05 19:40:08 +0300

    made changes in the urls

--- certapp/urls.py
+++ certapp/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
 
-app_name = 'certapp'
-
 urlpatterns = [
     path('', views.index, name='index'),
     path('verification', views.verification, name='verification'),
+    path('register', views.register, name='register'),
+    path('login', views.login, name='login'),
 ]
